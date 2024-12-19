4957aaf23b1186ae5604e76187cf9022f5669d39
Antônio Irineu Filho <161607435+AntonioIrineuFilho@users.noreply.github.com>
2024-12-19 20:29:33 -0300

    versão final

--- Unidade 03/tarefa03.c
+++ Unidade 03/tarefa03.c
@@ -1,11 +1,30 @@
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
 const uint button = 11; // GP11
 const uint ledRed = 4; // GP04
 const uint ledYellow = 5; // GP05
 const uint ledGreen = 6; // GP06
 const uint ledGreenPed = 10; // GP10 
+const uint buzzer = 12; // GP12
+const uint buzzerFreq = 2000; // frequência do buzzer
 
+void  inicializarPWM(uint buzzer, uint buzzerFreq) {
+  gpio_set_function(buzzer, GPIO_FUNC_PWM); // definir o GP12 como saída do PWM
+  uint valorSlice = pwm_gpio_to_slice_num(buzzer); // pega o número de uma unidade de controle do PWM
+  pwm_config padrao = pwm_get_default_config(); // salva a configuração padrão do PWM
+  pwm_config_set_clkdiv(&padrao, clock_get_hz(clk_sys) / (buzzerFreq * 4096)); // realiza a divisão do clock, guardando no endereço de memoria padrão a divisão do clock do sistema pelo produto da frquência do buzzer com a resolução do PWM
+  pwm_init(valorSlice, &padrao, true); // inicializa o PWM
+  pwm_set_gpio_level(buzzer, 0); // inicia o PWM sem som 
+}
+
+void som(uint buzzer) {
+  uint valorSlice = pwm_gpio_to_slice_num(buzzer);
+  pwm_set_gpio_level(buzzer, 2048); // coloca o PWM na metade do nível
+  sleep_ms(15000); // define o tempo de duração do som
+  pwm_set_gpio_level(buzzer, 0); // encerra o som
+}
 
 void semaforo(uint red, uint yellow, uint green, uint greenPed, bool r, bool y, bool g, bool gped) {
   gpio_put(red, r);
@@ -13,6 +32,8 @@ void semaforo(uint red, uint yellow, uint green, uint greenPed, bool r, bool y, 
   gpio_put(green, g);
   gpio_put(greenPed, gped);
 }
+
+void semaforoPedestres(uint red, uint yellow, uint green, uint greenPed);
 
 void semaforoVeiculos(uint red, uint yellow, uint green, uint greenPed, uint button) {
   semaforo(red, yellow, green, greenPed, 0, 0, 1, 0);
@@ -45,7 +66,7 @@ void semaforoPedestres(uint red, uint yellow, uint green, uint greenPed) {
   semaforo(red, yellow, green, greenPed, 0, 1, 0, 0);
   sleep_ms(5000);
   semaforo(red, yellow, green, greenPed, 1, 0, 0, 1);
-  sleep_ms(15000);
+  som(buzzer);
 }
 
 void setup() {
@@ -55,6 +76,7 @@ void setup() {
   gpio_init(ledYellow);
   gpio_init(ledGreen);
   gpio_init(ledGreenPed);
+  gpio_init(buzzer);
 
   gpio_pull_up(button); // estabiliza a tensão do botão
 
@@ -65,6 +87,7 @@ void setup() {
   gpio_set_dir(ledYellow, GPIO_OUT);
   gpio_set_dir(ledGreen, GPIO_OUT);
   gpio_set_dir(ledGreenPed, GPIO_OUT);
+  gpio_set_dir(buzzer, GPIO_OUT);
 
   // definindo os LEDs inicialmente com o estado "desligado"
   gpio_put(ledRed, 0);
@@ -75,6 +98,7 @@ void setup() {
 
 int main () {
    setup();
+   inicializarPWM(buzzer, buzzerFreq);
 
   while (true) {
     semaforoVeiculos(ledRed, ledYellow, ledGreen, ledGreenPed, button);
