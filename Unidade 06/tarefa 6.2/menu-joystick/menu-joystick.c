33f7829da1434557854e0d590591bcabf1470df8
Antônio Irineu Filho <20241014040008@ifrn.local>
2025-02-06 13:59:13 -0300

    inicio setup e joystick

--- Unidade 06/tarefa 6.2/menu-joystick/menu-joystick.c
+++ Unidade 06/tarefa 6.2/menu-joystick/menu-joystick.c
@@ -9,6 +9,9 @@
 #define PINO_SDA 15
 #define VRX 26
 #define VRY 27
+#define ADC_CHANNEL_X 1
+#define ADC_CHANNEL_Y 0
+#define JOYSTICK_BTN 22
 
 ssd1306_t display;
 
@@ -28,6 +31,9 @@ void setup(){
     adc_init();
     adc_gpio_init(VRX);
     adc_gpio_init(VRY);
+    gpio_init(JOYSTICK_BTN);
+    gpio_set_dir(JOYSTICK_BTN, GPIO_IN);
+    gpio_pull_up(JOYSTICK_BTN);
 }
 
 void draw(char *text, int row, bool clear) {
@@ -44,5 +50,27 @@ void clear() {
 
 int main() {
     setup();
+    int selected = 1;
+    while(true) {
+        adc_select_input(ADC_CHANNEL_Y); // Seleciona o eixo Y do joystick como input
+        uint adc_y_raw = adc_read(); // lê o ADC para o eixo Y
+        if (adc_y_raw > 4000) {
+            if (selected == 1) {
+                selected++;
+            } else if (selected == 2) {
+                selected++;
+            } else if (selected == 3) {
+                selected = 1;
+            }
+        } else if (adc_y_raw < 100) {
+            if (selected == 2) {
+                selected--;
+            } else if (selected == 3) {
+                selected--;
+            }
+        }
+
+    }
+
 
 }
