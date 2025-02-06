438f377f776315c007bc7d0177125bf41231e9bd
Antonio Irineu Filho <antonioirineu92@gmail.com>
2025-02-06 18:10:08 -0300

    desenvolvimento da lógica do menu usando o joystick

--- Unidade 06/tarefa 6.2/menu-joystick/menu-joystick.c
+++ Unidade 06/tarefa 6.2/menu-joystick/menu-joystick.c
@@ -36,36 +36,45 @@ void setup(){
     gpio_pull_up(JOYSTICK_BTN);
 }
 
-void draw(char *text, int row, bool clear) {
-    if (clear) {
-        ssd1306_clear(&display);
-    }
-    ssd1306_draw_string(&display, 8, row, 1, text);
+void draw(char *text, int col, int row) {
+    ssd1306_draw_string(&display, col, row, 1, text);
     ssd1306_show(&display);
 }
 
-void clear() {
-    ssd1306_clear(&display);
+void menu(int row, bool clear) {
+    if (clear) {
+        ssd1306_clear(&display);
+    }
+    draw("-> ", 8, row);
+    draw("JOYSTICK LED", 13, 10);
+    draw("TOCAR BUZZER", 13, 30);
+    draw("LIGAR RGB", 13, 50);    
 }
 
 int main() {
     setup();
+    menu(10, true);
     int selected = 1;
     while(true) {
         adc_select_input(ADC_CHANNEL_Y); // Seleciona o eixo Y do joystick como input
         uint adc_y_raw = adc_read(); // lê o ADC para o eixo Y
         if (adc_y_raw > 4000) {
             if (selected == 1) {
+                menu(30, true);
                 selected++;
             } else if (selected == 2) {
+                menu(50, true);
                 selected++;
             } else if (selected == 3) {
+                menu(10, true);
                 selected = 1;
             }
         } else if (adc_y_raw < 100) {
             if (selected == 2) {
+                menu(10, true);
                 selected--;
             } else if (selected == 3) {
+                menu(30, true);
                 selected--;
             }
         }
